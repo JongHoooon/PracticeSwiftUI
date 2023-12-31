7f261d1b4af35379f0e28ba2e011a812c35e69b9
JongHoon <qnm541@icloud.com>
2023-12-31 22:34:02 +0900

    hike - setup project

---
+++ Hike/Hike/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by JongHoon on 12/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Image(.image1)
+      .resizable()
+      .scaledToFit()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by JongHoon on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
