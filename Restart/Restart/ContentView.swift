734735fa1bef6ca1b8dc78160d94e46237017f9f
JongHoon <qnm541@icloud.com>
2024-01-18 22:37:33 +0900

    creat Restart

---
+++ Restart/Restart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by JongHoon on 1/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by JongHoon on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
